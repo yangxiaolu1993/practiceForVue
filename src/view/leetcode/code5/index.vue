09c55652ffb0c1a91dcc1dacf871d35de8e1d548
yangxiaolu3 <yangxiaolu3@jd.com>
2021-06-23 11:39:58 +0800

    提交

--- src/view/leetcode/code5/index.vue
+++ src/view/leetcode/code5/index.vue
@@ -9,14 +9,17 @@
 
     <div class="lc-chunk">
       <div class="chunk-title">名词解释</div>
-      <div>回文子串：字符串正序读的结果与倒序读的结果一样，例如“上海自来水来自海上”</div>
+      <div>
+        回文子串：字符串正序读的结果与倒序读的结果一样，例如“上海自来水来自海上”
+      </div>
     </div>
-    
+
     <div class="lc-chunk">
       <div class="chunk-title">知识点</div>
-      <div>回文子串：字符串正序读的结果与倒序读的结果一样，例如“上海自来水来自海上”</div>
+      <div>
+        回文子串：字符串正序读的结果与倒序读的结果一样，例如“上海自来水来自海上”
+      </div>
     </div>
-    
   </div>
 </template>
 <script>
@@ -28,37 +31,48 @@ export default {
     };
   },
   mounted() {
-    let s = "babad";
-    let s1 = "babadcbbd"
-    // console.log(s.substring(1, 3));
-    console.log(this.solve(s1));
+    let s = "mqizdjrfqtmcsruvvlhdgzfrmxgmmbguroxcbhalzggxhzwfznfkrdwsvzhieqvsrbyedqxwmnvovvnesphgddoikfwuujrhxwcrbttfbmlayrlmpromlzwzrkjkzdvdkpqtbzszrngczvgspzpfnvwuifzjdrmwfadophxscxtbavrhfkadhxrmvlmofbzqshqxazzwjextdpuszwgrxirmmlqitjjpijptmqfbggkwaolpbdglmsvlwdummsrdyjhmgrasrblpjsrpkkgknsucsshjuxunqiouzrdwwooxclutkrujpfebjpoodvhknayilcxjrvnykfjhvsikjabsdnvgguoiyldshbsmsrrlwmkfmyjbbsylhrusubcglaemnurpuvlyyknbqelmkkyamrcmjbncpafchacckhymtasylyfjuribqxsekbjkgzrvzjmjkquxfwopsbjudggnfbuyyfizefgxamocxjgkwxidkgursrcsjwwyeiymoafgyjlhtcdkgrikzzlenqgtdukivvdsalepyvehaklejxxmmoycrtsvzugudwirgywvsxqapxyjedbdhvkkvrxxsgifcldkspgdnjnnzfalaslwqfylmzvbxuscatomnmgarkvuccblpoktlpnazyeazhfucmfpalbujhzbykdgcirnqivdwxnnuznrwdjslwdwgpvjehqcbtjljnxsebtqujhmteknbinrloregnphwhnfidfsqdtaexencwzszlpmxjicoduejjomqzsmrgdgvlrfcrbyfutidkryspmoyzlgfltclmhaeebfbunrwqytzhuxghxkfwtjrfyxavcjwnvbaydjnarrhiyjavlmfsstewtxrcifcllnugldnfyswnsewqwnvbgtatccfeqyjgqbnufwttaokibyrldhoniwqsflvlwnjmffoirzmoxqxunkuep";
+    let s1 = "babadcbbd";
+    // console.log(this.isPalindrome(s1));
+    console.log(this.solve(s));
   },
   methods: {
+
+    /**
+     * 方案一：执行没问题，但在 LC 中提交显示“超出时间限制...”
+     */
     solve(s) {
-      if (s == '') return s;
+      if (s == "") return s;
       let n1 = 0;
-      let n2 = 1;
+      let n2 = s.length - 1;
       let maxL = s.substring(n1, 1);
 
-      while (n1 < s.length) {
+      while (n1 < s.length - 1) {
+        
         if (s[n2] == s[n1]) {
-          if (maxL.length < n2 - n1 + 1) {
-            maxL = s.substring(n1, n2 + 1);
-          }
+          let sChild = s.substring(n1, n2 + 1);
 
-          n1 += 1;
-          n2 = n1 + 1;
-        } else {
-          if (n2 < s.length - 2) {
-            n2 += 1;
-          } else {
+          if (this.isPalindrome(sChild)){
+            if(maxL.length < sChild.length) maxL = sChild;
             n1 += 1;
-            n2 = n1 + 1;
+            n2 = s.length - 1;
+          } else {
+            n2 -= 1;
           }
+        } else {
+          n2 -= 1;
         }
       }
 
       return maxL;
+    },
+    /**
+     * 判断当前字符串是否是回文字符串
+     * 思路一：将字符串反转，判断两个字符串是否一样
+     */
+    isPalindrome(s) {
+      var reverseS = s.split("").reverse().join("");
+      return s == reverseS;
     },
   },
 };
